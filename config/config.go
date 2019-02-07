d0ea766c59ced67e8fdccb02c2d86471e3155ea2
Christopher Baklid <christopherb@unity3d.com>
2019-02-07 14:31:29 +0100

    go fmt

--- config/config.go
+++ config/config.go
@@ -1,11 +1,12 @@
 package config
 
 import (
-    "gopkg.in/yaml.v2"
-    "log"
-    "io/ioutil"
-    "os"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"os"
 )
+
 /*
 
 this site is awesome: https://transform.now.sh/json-to-go/
@@ -18,30 +19,27 @@ twitter:
 */
 
 type Config struct {
-    Twitter struct {
-        Track     []string `yaml:"track"`
-        Follow    []string `yaml:"follow"`
-        Blacklist []string `yaml:"blacklist"`
-    } `yaml:"twitter"`
+	Twitter struct {
+		Track     []string `yaml:"track"`
+		Follow    []string `yaml:"follow"`
+		Blacklist []string `yaml:"blacklist"`
+	} `yaml:"twitter"`
 }
 
-func ReadConfig() (Config) {
-    b, err := ioutil.ReadFile("./config.yml")
-    if err != nil {
-        log.Println("Cannot find config.yml")
-        log.Println("copy config_example.yml to config.yml and try again")
-        os.Exit(1)
-    }
+func ReadConfig() Config {
+	b, err := ioutil.ReadFile("./config.yml")
+	if err != nil {
+		log.Println("Cannot find config.yml")
+		log.Println("copy config_example.yml to config.yml and try again")
+		os.Exit(1)
+	}
 
-    var cfg Config
-    if err := yaml.Unmarshal(b, &cfg); err != nil {
-        log.Fatal(err)
-    }
-    log.Println("Loading config config.yml")
-    log.Println(cfg)
+	var cfg Config
+	if err := yaml.Unmarshal(b, &cfg); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Loading config config.yml")
+	log.Println(cfg)
 
-    return cfg
+	return cfg
 }
-
-
-
